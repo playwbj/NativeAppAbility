7842719c2ec4e8affee395ff1f44166adffebb68
jeiiy.wang <jeiiy.wang@wingconn.com>
2018-12-28 14:11:17 +0800

    amend the core logic for ios

--- src/ios/NativeAppAbility.swift
+++ src/ios/NativeAppAbility.swift
@@ -8,6 +8,35 @@
 
 import Foundation
 
-class NativeAppAbility: CDVPlugin {
+@objc(NativeAppAbility) class NativeAppAbility: CDVPlugin {
     
+    //check if app is installed
+    func checkAppInstalled(command: CDVInvokedUrlCommand){
+        var pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: true);
+        let uriArg = command.argument(at: 0) as! String;
+        let url = URL.init(string: uriArg);
+        
+        let isExsited = UIApplication.shared.canOpenURL(url!);
+        if isExsited {
+            return self.commandDelegate.send(pluginResult, callbackId: command.callbackId);
+        }else{
+            pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: false);
+            return self.commandDelegate.send(pluginResult, callbackId: command.callbackId);
+        }
+    }
+    
+    //startup the native app    
+    func startupNativeApp(command: CDVInvokedUrlCommand){
+        var pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: true);
+        let uriArg = command.argument(at: 0) as! String;
+        let url = URL.init(string: uriArg);
+        
+        let isOpened = UIApplication.shared.openURL(url!);
+        if isOpened {
+            return self.commandDelegate.send(pluginResult, callbackId: command.callbackId);
+        }else{
+            pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: false);
+            return self.commandDelegate.send(pluginResult, callbackId: command.callbackId);
+        }
+    }
 }
