392d963ea41ab39381d87750da529f5fa81ed508
jeiiy.wang <jeiiy.wang@wingconn.com>
2018-12-28 11:59:33 +0800

    add ios support

---
+++ src/ios/NativeAppAbility.swift
@@ -0,0 +1,13 @@
+//
+//  NativeAppAbility.swift
+//  MyTestApp1
+//
+//  Created by wingconn on 2018/12/27.
+//  Copyright © 2018 wingconn-研发部. All rights reserved.
+//
+
+import Foundation
+
+class NativeAppAbility: CDVPlugin {
+    
+}
