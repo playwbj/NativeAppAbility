4f12772b40c650f3fd5cb90c198c5bc9ff70176d
playwbj <playwbj@163.com>
2018-12-29 09:29:24 +0800

    Update NativeAppAbility.swift

--- src/ios/NativeAppAbility.swift
+++ src/ios/NativeAppAbility.swift
@@ -3,7 +3,7 @@
 //  MyTestApp1
 //
 //  Created by wingconn on 2018/12/27.
-//  Copyright © 2018 wingconn-研发部. All rights reserved.
+//  Copyright © 2018 wingconn. All rights reserved.
 //
 
 import Foundation
