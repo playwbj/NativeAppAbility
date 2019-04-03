8d02bede41c53062a311b4a6a02fffbf0c4e9f0a
perkins.wu <perkins.wu@wingconn.com>
2019-04-03 14:27:43 +0800

    add the neccessary file

---
+++ www/nativeAppAbility.js
@@ -0,0 +1,24 @@
+var nativeAppAbility = {    
+    checkAppInstalled: function(urlScheme, successCallback, errorCallback){
+        cordova.exec(
+            successCallback,
+            errorCallback,
+            "nativeAppAbility",
+            "checkAppInstalled",
+            [urlScheme]
+        );
+    },
+    
+    startupNativeApp: function(urlScheme, successCallback,errorCallback){
+        cordova.exec(
+            successCallback,
+            errorCallback,
+            "nativeAppAbility",
+            "startupNativeApp",
+            [urlScheme]
+        );
+    }
+    
+};
+
+module.exports = nativeAppAbility;
