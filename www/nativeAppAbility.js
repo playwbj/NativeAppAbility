7f1b91f0a973d59338d7be13bd0e088b75ee20aa
playwbj <playwbj@163.com>
2019-05-26 22:38:50 +0800

    Update nativeAppAbility.js
    add new feature listen to broadcast for android platform

--- www/nativeAppAbility.js
+++ www/nativeAppAbility.js
@@ -16,27 +16,47 @@ var nativeAppAbility = {
             var errorCallback = arguments[2];
 
             cordova.exec(
-                        successCallback,
-                        errorCallback,
-                        "nativeAppAbility",
-                        "startupNativeApp",
-                        [urlScheme]
-                    );
+                successCallback,
+                errorCallback,
+                "nativeAppAbility",
+                "startupNativeApp",
+                [urlScheme]
+            );
         }else if (arguments.length == 5){
             var urlScheme = arguments[0];
             var userId = arguments[1];
-            var sign = arguments[2]
+            var sign = arguments[2];
             var successCallback = arguments[3];
             var errorCallback = arguments[4];
 
             cordova.exec(
-                        successCallback,
-                        errorCallback,
-                        "nativeAppAbility",
-                        "startupNativeApp",
-                        [urlScheme, userId, sign]
-                    );
+                successCallback,
+                errorCallback,
+                "nativeAppAbility",
+                "startupNativeApp",
+                [urlScheme, userId, sign]
+            );
         }
+    },
+
+    initListener: function(){
+        var successCallback = function(info){
+             cordova.fireWindowEvent('appinstalled', info);
+        };
+
+        var errorCallback = function(info){
+             console.log('error occured...');
+        };
+        
+        cordova.addWindowEventHandler('appinstalled').onHasSubscribersChange = function(){
+            cordova.exec(
+                successCallback,
+                errorCallback,
+                "nativeAppAbility",
+                "appInstalled",
+                []
+            );
+        };
     }
 };
 
