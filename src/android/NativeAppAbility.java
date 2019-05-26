bead289de1c8f8dc30a213f4a0845ca3a5939a15
playwbj <playwbj@163.com>
2019-05-26 22:37:46 +0800

    Update NativeAppAbility.java
    add new feature for listen the broadcast

--- src/android/NativeAppAbility.java
+++ src/android/NativeAppAbility.java
@@ -2,13 +2,17 @@ package com.wingconn;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.LOG;
+import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageInfo;
 import android.content.pm.ResolveInfo;
@@ -18,6 +22,10 @@ import android.os.Bundle;
 import java.util.List;
 
 public class NativeAppAbility extends CordovaPlugin {
+
+    BroadcastReceiver receiver  = null;
+    private CallbackContext callbackContext = null;
+
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if(action.equals("checkAppInstalled")) {
@@ -34,6 +42,30 @@ public class NativeAppAbility extends CordovaPlugin {
             }
             this.startupNativeApp(uri, userId,sign, callbackContext);
             return true;
+        }else if(action.equals("appInstalled")) {
+            if (this.callbackContext != null) {
+                removeListener();
+            }
+            this.callbackContext = callbackContext;
+
+            IntentFilter intentFilter = new IntentFilter();
+            intentFilter.addAction(Intent.ACTION_PACKAGE_ADDED);
+            intentFilter.addDataScheme("package");
+            if (this.receiver == null) {
+                this.receiver = new BroadcastReceiver() {
+                    @Override
+                    public void onReceive(Context context, Intent intent) {
+                        updateListenerInfo(intent);
+                    }
+                };
+                webView.getContext().registerReceiver(this.receiver, intentFilter);
+            }
+
+            // Don't return any result now, since status results will be sent when events come in from broadcast receiver
+            PluginResult pluginResult = new PluginResult(PluginResult.Status.NO_RESULT);
+            pluginResult.setKeepCallback(true);
+            callbackContext.sendPluginResult(pluginResult);
+            return true;
         }
 //        else if(action.equals("startupAppWithParams")) {
 //            String uri = args.getString(0);
@@ -46,6 +78,54 @@ public class NativeAppAbility extends CordovaPlugin {
 //            return true;
 //        }
         return false;
+    }
+
+    /**
+     *
+     * @param info
+     * @param keepCallback
+     */
+    private void sendUpdate(JSONObject info, boolean keepCallback) {
+        if (this.callbackContext != null) {
+            PluginResult result = new PluginResult(PluginResult.Status.OK, info);
+            result.setKeepCallback(keepCallback);
+            this.callbackContext.sendPluginResult(result);
+        }
+    }
+
+    private JSONObject getInstalledInfo(Intent intent) {
+        JSONObject obj = new JSONObject();
+        try {
+            String part = intent.getData().getSchemeSpecificPart();
+            String dataStr = intent.getDataString();
+            obj.put("packageName",part);
+            obj.put("packageName",dataStr);
+        }catch (JSONException e){
+            e.printStackTrace();
+        }
+        return obj;
+    }
+
+    /**
+     *
+     * @param installIntent
+     */
+    private void updateListenerInfo(Intent installIntent) {
+        sendUpdate(this.getInstalledInfo(installIntent), true);
+    }
+
+    /**
+     * remove the listener which already registered
+     */
+    private void removeListener() {
+        if (this.receiver != null) {
+            try {
+                webView.getContext().unregisterReceiver(this.receiver);
+                this.receiver = null;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 
